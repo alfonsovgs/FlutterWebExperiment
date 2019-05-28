1bce922a64ad9dd1807a054d7581cbea9045f533
Alfonso Vargas <vc.alfonso@hotmail.com>
2019-05-27 22:43:22 -0500

    Add drawer and scoffold

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,10 +1,41 @@
 import 'package:flutter_web/material.dart';
+import 'package:portafolio/responsive_widget.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ResponsiveWidget(
+      largeScreen: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: Colors.black,
+        ),
+        drawer: ResponsiveWidget.isSmallScreen(context)
+            ? Drawer(
+                child: ListView(
+                  padding: EdgeInsets.all(20),
+                  children: <Widget>[Icon(Icons.access_alarms)],
+                ),
+              )
+            : null,
+        body: SingleChildScrollView(
+          child: AnimatedPadding(
+            duration: Duration(seconds: 1),
+            padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.1),
+            child: ResponsiveWidget(
+              largeScreen: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text("Hola"),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
